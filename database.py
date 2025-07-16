dd511f4d7d7f856e7cb5a4d9aa2a8c413f6a25c1
SUKUNA-AI <ilyakoryaev@yandex.ru>
2025-07-16 17:28:58 +0300

    Добавлены подключения к БД

--- database.py
+++ database.py
@@ -1,3 +1,13 @@
 """
 Настраивает SQLAlchemy для использования базы данных PostgresSQL
 """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qwerty12345@localhost:5432/fantasy_data"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
